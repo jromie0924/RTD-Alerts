0e08361daf97dc203a9860ba7203c0949bdb83dd
Jackson <jromie0924@gmail.com>
2019-03-31 21:51:35 -0600

    remove unused imports from app.js

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-const http = require("http");
-const mail = require("./emailer.js")
 const auth = require("./auth.js");
-const dbUpdater = require("./db-updater.js");
-const db = require("./db.js");
 const controller = require("./controller.js");
 
 controller.createServer();
