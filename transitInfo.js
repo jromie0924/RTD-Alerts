62741722dff30001d5036275cc1ea97149498ec6
Jackson <jromie0924@gmail.com>
2019-04-10 17:52:25 -0600

    Twitter responds to API requests in app

--- transitInfo.js
+++ transitInfo.js
@@ -2,6 +2,8 @@ const https = require('https');
 const auth = require('./auth.js');
 const db = require('./db.js');
 
+const twitterUrl = 'https://api.twitter.com';
+
 exports.watchTransit = (callback) => {
   db.getAllUsers((err, users) => {
     if (err) {
@@ -9,13 +11,49 @@ exports.watchTransit = (callback) => {
     } else {
       users.forEach(user => {
         const transit = user.transit;
-        db.getQueryParams(transit, (err, docs) => {
+        db.getQueryParams(transit, (err, data) => {
           if (err) {
             callback(JSON.parse(err));
           } else {
-            console.log(docs);
+            const params = data[0];
+            const screenName = params.screen_name;
+            const count = params.count;
+            getData(screenName, count, (err, res) => {
+              if (err) {
+                console.error(err);
+              } else {
+                console.log(JSON.parse(res));
+              }
+            })
           }
         });
+      });
+    }
+  });
+}
+
+var getData = (screenName, count, callback) => {
+  auth.authenticate((err, token) => {
+    if (err) {
+      callback(JSON.parse(err));
+    } else {
+      const options = {
+        path: `/1.1/statuses/user_timeline.json?screen_name=${screenName}&count=${count}&exclude_replies=true&tweet_mode=extended`,
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      };
+      https.get(twitterUrl, options, res => {
+        if (res.statusCode === 200) {
+          res.on('data', data => {
+            const posts = JSON.parse(data);
+            posts.forEach(post => {
+              const text = post.full_text;
+              // TODO: Look for keywords in text
+            });
+          });
+        }
       });
     }
   });
