fb86cf081dfa9783bea76e85c10cff495d7deafd
Jackson <jromie0924@gmail.com>
2020-03-15 23:44:09 -0600

    DB registration

--- RTD-Alerts/Context/RTDAlertsContext.cs
+++ RTD-Alerts/Context/RTDAlertsContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using RTD_Alerts.Models;
 
 namespace RTD_Alerts.Context
 {
@@ -7,6 +8,19 @@ namespace RTD_Alerts.Context
         public RTDAlertsContext(DbContextOptions<RTDAlertsContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Keyword> Keywords { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<TransitAuth> TransitAuths { get; set; }
+        public DbSet<TweetHistory> TweetHistory { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Keyword>().ToTable("Keywords");
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<TransitAuth>().ToTable("Transit_Auth");
+            modelBuilder.Entity<TweetHistory>().ToTable("Tweet_History");
         }
     }
 }
--- RTD-Alerts/Models/Keyword.cs
+++ RTD-Alerts/Models/Keyword.cs
@@ -2,6 +2,7 @@ namespace RTD_Alerts.Models
 {
     public class Keyword
     {
+        int id { get; set; }
         public string Value { get; set; }
     }
 }
